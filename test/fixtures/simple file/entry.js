b08b10011de995677cdda53d28a70336b78505bb
Ben Ng <me@benng.me>
2013-08-19 21:08:28 -0600

    Fix simple file test

--- test/fixtures/simple file/entry.js
+++ test/fixtures/simple file/entry.js
@@ -5,7 +5,7 @@ var myString = ['Open','Your','Dev','Tools!']
 
 anotherString = myString.join(' ');
 
-header.innerHTML = myString;
+header.innerHTML = anotherString;
 document.body.appendChild(header);
 
 msg.innerHTML = 'Do you see this? Great! Too bad this isn\'t actually the test.<br /><br />Open your dev tools and see if an "entry.js" file is there. If so, the actual test suceeded.<br /><br /><em>Remember to turn on source maps in your options!</em>';
