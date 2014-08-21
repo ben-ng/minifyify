c779e7834055fa74918c67bc81f2148b9644746d
Ben Ng <me@benng.me>
2014-08-20 20:15:17 -0700

    Fix #57

--- test/fixtures/complex file/entry.js
+++ test/fixtures/complex file/entry.js
@@ -1,4 +1,5 @@
 var submodule = require('./submodule')
+  , jsonthing = require('./jsonthing.json')
   , myString
   , actual
   , expected;
@@ -8,14 +9,14 @@ myString = submodule.createString(function () {
 
   mathy *= 1337;
 
-  return 'potato #' + mathy;
+  return 'potato #' + mathy + jsonthing.turkey;
 });
 
 actual = document.createElement('pre');
 expected = document.createElement('pre');
 
 actual.innerHTML   = 'Actual:   ' + myString;
-expected.innerHTML = 'Expected: Wed Dec 31 1969 22:30:23 GMT-0800 (PST) friedpotato #26740bakedpotato #26740sliced potato #26740';
+expected.innerHTML = 'Expected: Wed Dec 31 1969 22:30:23 GMT-0800 (PST) friedpotato #26740salmonbakedpotato #26740salmonsliced potato #26740salmon';
 
 document.body.appendChild(actual);
 document.body.appendChild(expected)
